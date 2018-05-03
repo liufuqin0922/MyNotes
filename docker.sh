5c45558967fcbebd5088fe460f6399a93c4f3d5b
MacBookPro <windforce@Mac.local>
2018-05-03 08:11:17 +0800

    docker

---
+++ docker.sh
@@ -0,0 +1,17 @@
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common &&
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add - &&
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" &&
+sudo apt-get update &&
+sudo apt-get install docker-ce &&
+sudo echo -e "{\n 
+  \"registry-mirrors\": [\"https://docker.mirrors.ustc.edu.cn/\"]
+}" >/etc/docker/daemon.json 
+sudo systemctl restart docker
